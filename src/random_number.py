cf7ca4f3136810bf781d112c2e4f7add32ecdbec
Mane.Tejesh <manetejesh12@outlook.com>
2024-12-15 13:07:33 +0530

    updated file

--- src/random_number.py
+++ src/random_number.py
@@ -1,33 +1,30 @@
 import random
 
-top_of_range = input("Type a number: ")
+top_of_range = input("type a number for top of range?")
 
 if top_of_range.isdigit():
     top_of_range = int(top_of_range)
 
-    if top_of_range <= 0:
-        print("Enter number greater then 0 next time")
+    if top_of_range < 0:
+        print("enter number greater than 0")
         quit()
 else:
-    print("Type a number next time")
+    print("Enter number next time")
     quit()
 
-random_number = random.randint(0, top_of_range)
-print(random_number)
-guess = 0
-while True:
-    guess += 1
-    user_guess = input("Enter you user_guess: ")
-    if user_guess.isdigit():
-        user_guess = int(user_guess)
-    else:
-        print("Type a number next time")
-        continue
-    if user_guess == random_number:
-        print("You got it!")
-        break
-    elif user_guess > random_number:
-        print("you were above the number")
-    else:
-        print("you were below the number")
-print(f"you guess the numebr in {guess} guess")
+
+# game_on = input("would you like to play game?")
+# print(game_on)
+
+# if game_on.lower() == "yes":
+#     print("started the game")
+#     number_range = input("select the range of number?")
+#     random_num = random.randint(0, int(number_range))
+#     print(random_num)
+
+# else:
+#     print("game will not start")
+
+
+# num = random.randint(0, 10)
+# print(num)
